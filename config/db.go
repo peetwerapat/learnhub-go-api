60047a9633ecbc0756c1946579ff95311d1dab35
peetwerapat <peetwerapat.dev@gmail.com>
2025-08-20 10:01:56 +0700

    SetUp DB and Test Swagger

---
+++ config/db.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() error {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	err = database.AutoMigrate()
+	if err != nil {
+		return fmt.Errorf("failed to migrate database: %w", err)
+	}
+
+	DB = database
+	return nil
+}
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @title My API
+// @version 1.0
+// @description This is a sample server.
+// @host localhost:8080
+// @BasePath /
 func main() {
 	r := gin.Default()
 
